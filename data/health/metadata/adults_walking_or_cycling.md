1865fa54a40126c3367ba36868a54455b2939fa8
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-08-15 17:01:30 +0100

    Change walking and cycling data to NHS peer group and metadata

--- data/health/metadata/adults_walking_or_cycling.md
+++ data/health/metadata/adults_walking_or_cycling.md
@@ -2,10 +2,10 @@
 
 Note: the following links open in a new window.
 
-Source: Dataset CW0301: <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1181185/cw0301-proportion-of-adults-who-do-any-walking-or-cycling-by-frequency-purpose-and-local-authority.ods" target="_blank">Proportion of adults who do any walking or cycling, for any purpose, by frequency and local authority: England</a> from <a href="https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw" target="_blank">Department for Transport</a>.
+Source: Dataset CW0301: Proportion of adults who do any walking or cycling, for any purpose, by frequency and local authority: England, from <a href="https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw" target="_blank">Participation in walking and cycling. Department for Transport</a>.
 
 Further information regarding this dataset can be found in <a href="https://www.gov.uk/government/publications/walking-and-cycling-statistics-notes-and-definitions/walking-and-cycling-statistics-notes-and-definitions" target="_blank">Walking and cycling statistics: notes and definitions</a>
 
-Data for Trafford compared to the average of 15 similar authorities as defined by the Chartered Institute of Public Finance and Accountancy (CIPFA) in their <a href='https://www.cipfa.org/services/cipfastats/nearest-neighbour-model' target='_blank'>nearest neighbours model</a> from 2021.
+Data for Trafford compared to the average of 15 similar authorities from its NHS England Near Neighbour Peer Group.
 
-<a href="https://www.trafforddatalab.io/corporate_plan/data/climate/adults_walking_or_cycling.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
+<a href="https://www.trafforddatalab.io/trafford_themes/data/health/adults_walking_or_cycling.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
--- data/health/pre-processing/adults_walking_or_cycling.R
+++ data/health/pre-processing/adults_walking_or_cycling.R
@@ -1,5 +1,4 @@
 # Proportion of adults who do any walking or cycling, for any purpose, five times per week
-# Created: 2022-02-07  Updated: 2023-09-27  Data: 2023-08-30
 
 # Source: Department for Transport (DfT)
 #         https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw#participation-in-walking-and-cycling
@@ -10,8 +9,8 @@
 library(tidyverse); library(readODS); library(httr);
 
 # Setup objects ---------------------------
-# Trafford and its CIPFA nearest neighbours (2021):
-authorities <- read_csv("../../cipfa2021.csv") %>%
+# Trafford and its NHS pair groups :
+authorities <- read_csv("../../nhsennpg.csv") %>%
   add_row(area_code = "E08000009", area_name = "Trafford") %>%
   add_row(area_code = "E92000001", area_name = "England")
 
@@ -24,29 +23,17 @@ df_raw <- read_ods(tmp, sheet = "CW0301_Any_Purpose", skip = 4)
 
 # Tidy the data ---------------------------
 df_wlk_cyc <- df_raw %>%
-  rename(area_code = `ONS.Code`,
-         area_name = `Area.name`) %>%
+  rename(area_code = `ONS Code`,
+         area_name = `Area name`) %>%
   filter(Frequency == "At least 5 times per week",
          area_code %in% authorities$area_code) %>%
   select(-Class, -Mode, -Purpose, -Frequency) %>% # remove unwanted columns
-  rename(`2016` = `X2016`,
-         `2017` = `X2017`,
-         `2018` = `X2018`,
-         `2019` = `X2019`,
-         `2020` = `X2020`,
-         `2021` = `X2021`,
-         `2022` = `X2022`) %>%  
     mutate(`2021` = as.character(`2021`),
            `2022` = as.character(`2022`)) %>% # annoyingly these columns are num when the others are char - need all the same to pivot
   pivot_longer(cols = c(-area_code, -area_name),
                names_to = "period",
                values_to = "value") %>%
-  # the period needs to be in the format 20YY-YY.
-  # each year value in period is the latest, so we need to create the year before number
-  mutate(period_to = str_sub(period, -2, -1),
-         period_from = (as.numeric(period) -1),
-         period = paste0(period_from, "-", period_to),
-         value = round(as.numeric(value), 1),
+  mutate(value = round(as.numeric(value), 1),
          indicator = "Proportion of adults who do any walking or cycling, for any purpose, five times per week",
          measure = "Percentage",
          unit = "Persons") %>%
