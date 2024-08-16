1f8c5bd6a73f56104f394a0e82e90c2767d6a5be
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-08-16 10:52:54 +0100

    Adding business births and deaths and created metadata file

---
+++ data/economy/metadata/business_births_deaths.md
@@ -0,0 +1,10 @@
+#### Business Births and Deaths.
+
+Source: <a href='https://www.ons.gov.uk/businessindustryandtrade/business/activitysizeandlocation/datasets/businessdemographyquarterlyexperimentalstatisticslowlevelgeographicbreakdownuk' target='_blank'> Business demography, quarterly, UK, low-level geographic breakdown, UK. ONS</a>. <a href='https://www.nomisweb.co.uk/datasets/pestsyoala' target='_blank'>Population estimates ONS</a>.
+
+Rate per 1,000 population of number of Business biths and Deaths using Mid-Year Population Estimates for the corresponding year and 2022 population for 2023.
+
+
+Data for Trafford compared to the average of 15 similar authorities as defined by the Chartered Institute of Public Finance and Accountancy (CIPFA) in their <a href='https://www.cipfa.org/services/cipfastats/nearest-neighbour-model' target='_blank'>nearest neighbours model</a>.
+ 
+<a href="https://www.trafforddatalab.io/trafford_themes/data/economy/business_births_deaths.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
---
+++ data/economy/pre-processing/business_births_deahts.R
@@ -0,0 +1,43 @@
+# Business births and deaths 
+
+# Source: Annual Survey of Hours and Earnings, ONS
+# URL: https://www.ons.gov.uk/businessindustryandtrade/business/activitysizeandlocation/datasets/businessdemographyquarterlyexperimentalstatisticslowlevelgeographicbreakdownuk
+# Licence: Open Government Licence
+
+
+# Load required packages ---------------------------
+
+library(tidyverse) ; library(readxl)
+
+bm <- read_csv("../../cipfalga0724.csv") %>%
+  select(area_code)
+
+population  <- read_csv(paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_2002_1.data.csv?geography=",paste(c("E92000001",bm$area_code, "E08000009"), collapse = ','),"&date=latestMINUS6-latest&gender=0&c_age=200&measures=20100")) %>%
+  select(year = DATE_NAME, area_code = GEOGRAPHY, pop = OBS_VALUE) 
+  
+
+tmp <- tempfile(fileext = ".xlsx")
+
+GET(url = "https://www.ons.gov.uk/file?uri=/businessindustryandtrade/business/activitysizeandlocation/datasets/businessdemographyquarterlyexperimentalstatisticslowlevelgeographicbreakdownuk/quarter2apriltojune2024/finalq22024lowlevelgeobreakdown.xlsx",
+    write_disk(tmp))
+
+df <- set_names(sheets[3:12]) %>%
+  map_df(~ read_xlsx(path = tmp, sheet = .x, skip = 3), .id = "sheet") %>%
+  mutate(indicator = sub(" .*", "", sheet)) %>%
+  mutate(area_code = sub(" : .*", "", Geography), area_name = sub(".* : ", "", Geography)) %>%
+  select(-sheet,-Geography) %>%
+  pivot_longer(`Q1 2017`:`Q2 2024`, names_to = "period", values_to = "Count") %>%
+  filter(!is.na(Count)) %>%
+  mutate(year = as.numeric( sub(".* ", "", period))) %>%
+  filter(area_code %in% c("E92000001",bm$area_code, "E08000009")) %>%
+  left_join(population, by = c("area_code" = "area_code", "year" = "year")) %>%
+  arrange(area_code) %>%
+  fill(pop) %>%
+  mutate(`Per 1000 population` = round((Count/pop)*1000, 2)) %>%
+  mutate(indicator = paste0("Business ", indicator)) %>%
+  select(area_code,area_name,period,indicator,Count,`Per 1000 population`) %>%
+  pivot_longer(c(Count,`Per 1000 population`), names_to = "measure", values_to = "value")
+
+# Export the tidied data ---------------------------
+write_csv(df, "../business_births_deaths.csv")
+
