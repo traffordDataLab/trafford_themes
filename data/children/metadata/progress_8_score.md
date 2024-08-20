04faafb0c01bb020eb5111dd0df3ff9677ae056a
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-08-20 17:59:02 +0100

    Adding Progress 8 score to children theme data

---
+++ data/children/metadata/progress_8_score.md
@@ -0,0 +1,15 @@
+#### Average Progress 8 score 
+
+Progress 8 aims to capture the progress that pupils in a school make from the end of primary school to the end of key stage 4 (KS4)
+
+Years shown are academic years. Data for 2019/20 and 2020/21 not avalable, due to exam cancellation because of COVID-19.
+
+Source: <a href="https://www.gov.uk/government/collections/statistics-gcses-key-stage-4" target="_blank">Statistics: GCSEs (key stage 4), DfE</a>
+
+Data for Trafford compared to the average of 10 Children's Services Statistical Neighbours as defined in the <a href='https://www.gov.uk/government/publications/local-authority-interactive-tool-lait' target='_blank'>Children's Services Statistical Neighbour Benchmarking Tool</a>
+
+Indicator:
+
+* <a href="http://id.esd.org.uk/metricType/6016" target="_blank"> Average Progress 8 score </a>
+
+<a href="https://www.trafforddatalab.io/trafford_themes/data/children/progress_8_score.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
---
+++ data/children/pre-processing/progress_8_score.R
@@ -0,0 +1,43 @@
+# Average Attainment 8 score
+
+# Source: Department for Education.
+# URL: https://www.gov.uk/government/collections/statistics-gcses-key-stage-4
+# Licence: Open Government Licence
+
+
+# Load required packages ---------------------------
+library(tidyverse) 
+
+# Setup objects ---------------------------
+# Trafford and its Children's Services nearest neighbours:
+authorities <- read_csv("../../cssn.csv") %>%
+  add_row(area_code = "E08000009", area_name = "Trafford") %>%
+  add_row(area_code = "E92000001", area_name = "England")
+
+# API key for LGInform+
+lginform_key <- ""
+
+# Download and tidy the data ---------------------------
+
+df_raw <- read_csv(paste0("https://webservices.esd.org.uk/data.csv?metricType=6016&area=",paste(c("E92000001",authorities$area_code, "E08000009"), collapse = ','),"&period=latest:8&columnGrouping=period&rowGrouping=area&ApplicationKey=",lginform_key))
+
+# Tidy the LG Inform+ data ---------------------------
+df <- df_raw %>%
+  select(area_code = area,
+         everything(),
+         -`area label`,
+         -`area long label`) %>%
+  filter(area_code != "area") %>%
+  pivot_longer(c(-area_code), names_to = "period", values_to = "value") %>%
+  left_join(authorities, by = "area_code") %>%
+  mutate(measure = "Percentage",
+         unit = "Persons",
+         indicator = "Average Progress 8 score",
+         value = as.numeric(value),
+         period = str_replace(period, "\\s\\(academic\\)", "")
+  ) %>%
+  arrange(period, area_name) %>%
+  select(area_code, area_name, period, indicator, measure, unit, value)
+
+# Export the tidied data ---------------------------
+write_csv(df, "../progress_8_score.csv")
