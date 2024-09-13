110bc1bf2cccb75c7aa8b4893011ee73f3105f0c
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-09-13 17:40:15 +0100

    Adding health assessments to children's tab

---
+++ data/children/metadata/cla_health_assessment.md
@@ -0,0 +1,8 @@
+#### Percentage of children looked after who had their annual health assessment.
+
+
+Source: <a href="https://www.gov.uk/government/collections/statistics-looked-after-children" target="_blank">Children looked after in England including adoption, DfE</a>
+
+Data for Trafford compared to the average of 10 Children's Services Statistical Neighbours as defined in the <a href='https://www.gov.uk/government/publications/local-authority-interactive-tool-lait' target='_blank'>Children's Services Statistical Neighbour Benchmarking Tool</a>
+
+<a href="https://www.trafforddatalab.io/trafford_themes/data/children/cla_health_assessment.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
---
+++ data/children/pre-processing/cla_health_assessment.R
@@ -0,0 +1,43 @@
+# Children looked after who had their annual health assessment #
+
+# Source: Department for Education
+# URL: https://www.gov.uk/government/collections/statistics-looked-after-children
+# Licence: Open Government Licence v3.0
+
+
+
+# Load required packages ---------------------------
+library(tidyverse) 
+
+# Setup objects ---------------------------
+# Trafford and its Children's Services nearest neighbours:
+authorities <- read_csv("../../cssn.csv") %>%
+  add_row(area_code = "E08000009", area_name = "Trafford") %>%
+  add_row(area_code = "E92000001", area_name = "England")
+
+# API key for LGInform+
+lginform_key <- ""
+
+# Download and tidy the data ---------------------------
+
+df_raw <- read_csv(paste0("https://webservices.esd.org.uk/data.csv?metricType=15496&area=",paste(c("E92000001",authorities$area_code, "E08000009"), collapse = ','),"&period=latest:8&columnGrouping=period&rowGrouping=area&ApplicationKey=",lginform_key))
+
+# Tidy the LG Inform+ data ---------------------------
+df <- df_raw %>%
+  select(area_code = area,
+         everything(),
+         -`area label`,
+         -`area long label`) %>%
+  filter(area_code != "area") %>%
+  pivot_longer(c(-area_code), names_to = "period", values_to = "value") %>%
+  left_join(authorities, by = "area_code") %>%
+  mutate(measure = "Percentage",
+         unit = "Persons",
+         indicator = "Children looked after who had their annual health assessment",
+         value = as.numeric(value)
+  ) %>%
+  arrange(period, area_name) %>%
+  select(area_code, area_name, period, indicator, measure, unit, value)
+
+# Export the tidied data ---------------------------
+write_csv(df, "../cla_health_assessment.csv")
