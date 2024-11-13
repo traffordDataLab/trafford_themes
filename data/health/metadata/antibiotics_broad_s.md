df75aaa8d845a149ead2ddd2b1e3a8f671c0f2e2
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-11-13 13:44:06 +0000

    Adding antibiotics broad spectrum

---
+++ data/health/metadata/antibiotics_broad_s.md
@@ -0,0 +1,13 @@
+#### Broad Spectrum antibiotics as a % of total prescription items for oral antibiotics
+
+Broad spectrum antibiotics can be used when others have failed. However they should only be used where narrow spectrum antibiotics are not likely to be effective, as they can increase the risk of drug-resistant bacteria developing.
+
+Source: https://openprescribing.net/
+
+Data for Trafford compared England. Data for Trafford's NHS England Near Neighbour Peer Group not available for all individual authorities.
+
+Indicator:
+
+* <a href="https://openprescribing.net/measure/broad_spectrum/sicbl/02A/" target="_blank"> Broad Spectrum antibiotics as a % of total prescription items for oral antibiotics </a>
+
+<a href="https://www.trafforddatalab.io/trafford_themes/data/health/antibiotics_broad_s.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
---
+++ data/health/pre-processing/antibiotics_broad_s.R
@@ -0,0 +1,33 @@
+# Broad Spectrum antibiotics as a % of total prescribed#
+
+# Source: openprescribing.net
+# URL: https://openprescribing.net/
+# Licence: Open Government Licence v3.0
+
+# Load required packages ---------------------------
+library(tidyverse) 
+
+# Download and tidy the data ---------------------------
+
+#Data downloaded from https://openprescribing.net/. Under the "Area and Practice Dashboards" dropdown select Sub-ICB locations option, choose your area e. g. "NHS Trafford", under the plots choose "Antimicrobial stewardship" topic, search for "Prescription items for co-amoxiclav, cephalosporins and quinolones as a percentage of total prescription items for oral antibiotics (excluding antituberculosis drugs and antileprotic drugs)." and download the data. Repeat for National data
+
+df_raw_trafford <- read_csv("Antibiotic stewardship co-amoxiclav cephalosporins quinolones.csv")
+
+df_raw_england <- read_csv("Antibiotic stewardship co-amoxiclav cephalosporins quinolones-2.csv")
+
+df_raw <- bind_rows(df_raw_trafford,df_raw_england)
+
+# Tidy the data ---------------------------
+df <- df_raw %>%
+  mutate(area_name = ifelse(is.na(org_name), "England", "Trafford")) %>%
+  mutate(area_code = ifelse(area_name == "Trafford", "E08000009", "E92000001")) %>%
+  mutate(measure = "Percentage",
+         unit = "prescriptions",
+         indicator = "Broad spectrum antibiotics as a % of total prescribed",
+         value = round(ratio*100,1),
+         period = date) %>%
+  select(area_code, area_name, period, indicator, measure, unit, value)
+
+
+# Export the tidied data ---------------------------
+write_csv(df, "../antibiotics_broad_s.csv")
