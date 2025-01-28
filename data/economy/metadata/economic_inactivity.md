d36deb6fcc7555cc07c5c0fc076aad5f31ac2904
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2025-01-28 12:18:44 +0000

    Adding indicator 'Economic inactivity'

---
+++ data/economy/metadata/economic_inactivity.md
@@ -0,0 +1,12 @@
+#### % who are economically inactive amongst those aged 16-64.
+
+Source: Annual Population Survey (APS)
+
+
+Data for Trafford compared to the average of 15 similar authorities as defined by the Chartered Institute of Public Finance and Accountancy (CIPFA) in their <a href='https://www.cipfa.org/services/cipfastats/nearest-neighbour-model' target='_blank'>nearest neighbours model</a>.
+ 
+Indicator:
+
+* <a href="https://www.nomisweb.co.uk/query/construct/summary.asp?mode=construct&dataset=17&version=0" target="_blank"> % who are economically inactive - aged 16-64 </a>
+
+<a href="https://www.trafforddatalab.io/trafford_themes/data/economy/economic_inactivity.csv" aria-label="Download the data" class="downloadButton" target="_blank" download>Data <span class="fas fa-download"></span></a>
---
+++ data/economy/pre-processing/economic_inactivity.R
@@ -0,0 +1,25 @@
+# Economic Inactivity#
+
+# Source: Annual Population Survey, Office for National Statistics
+# URL: https://www.nomisweb.co.uk/query/construct/summary.asp?mode=construct&version=0&dataset=17
+# Licence: Open Government Licence v3.0
+
+library(tidyverse) 
+
+bm <- read_csv("../../cipfalga0724.csv") %>%
+  select(area_code)
+
+df  <- read_csv(paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_17_5.data.csv?geography=",paste(c("E92000001",bm$area_code, "E08000009"), collapse = ','),"&date=latest-latestMINUS13&variable=111&measures=20599,21001,21002,21003")) %>%
+  filter(MEASURES_NAME == "Variable") %>%
+  mutate(measure = "Percentage",
+         unit = "Persons") %>% 
+  select(area_code = GEOGRAPHY_CODE,
+         area_name = GEOGRAPHY_NAME,
+         indicator = VARIABLE_NAME,
+         period = DATE_NAME, measure, unit,
+         value = OBS_VALUE) %>%
+  unique()
+
+
+write_csv(df, "../economic_inactivity.csv")
+
