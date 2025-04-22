f5885d8ead6aa07b706b32003c091977313ff43a
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2025-04-22 14:04:13 +0100

    Update Business births and deaths

--- data/economy/pre-processing/business_births_deaths.R
+++ data/economy/pre-processing/business_births_deaths.R
@@ -18,15 +18,18 @@ population  <- read_csv(paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_20
 
 tmp <- tempfile(fileext = ".xlsx")
 
-GET(url = "https://www.ons.gov.uk/file?uri=/businessindustryandtrade/business/activitysizeandlocation/datasets/businessdemographyquarterlyexperimentalstatisticslowlevelgeographicbreakdownuk/quarter2apriltojune2024/finalq22024lowlevelgeobreakdown.xlsx",
+
+GET(url = "https://www.ons.gov.uk/file?uri=/businessindustryandtrade/business/activitysizeandlocation/datasets/businessdemographyquarterlyexperimentalstatisticslowlevelgeographicbreakdownuk/quarter4octobertodecember2024/finalq42024lowlevelgeobreakdown.xlsx",
     write_disk(tmp))
+
+sheets <- excel_sheets(tmp) 
 
 df <- set_names(sheets[3:12]) %>%
   map_df(~ read_xlsx(path = tmp, sheet = .x, skip = 3), .id = "sheet") %>%
   mutate(indicator = sub(" .*", "", sheet)) %>%
   mutate(area_code = sub(" : .*", "", Geography), area_name = sub(".* : ", "", Geography)) %>%
   select(-sheet,-Geography) %>%
-  pivot_longer(`Q1 2017`:`Q2 2024`, names_to = "period", values_to = "Count") %>%
+  pivot_longer(`Q4 2017`:`Q4 2024`, names_to = "period", values_to = "Count") %>%
   filter(!is.na(Count)) %>%
   mutate(year = as.numeric( sub(".* ", "", period))) %>%
   filter(area_code %in% c("E92000001",bm$area_code, "E08000009")) %>%
